098f65802d7d55fb7db83d2028406425d092996b
Tzutzu <103909491+tzutzuliu@users.noreply.github.com>
2022-06-14 01:15:39 +0800

    Add files via upload

---
+++ vote_test.sql
@@ -0,0 +1,204 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- 主機： 127.0.0.1
+-- 產生時間： 2022-06-13 11:26:25
+-- 伺服器版本： 10.4.24-MariaDB
+-- PHP 版本： 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `vote_test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `admins`
+--
+
+CREATE TABLE `admins` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `acc` varchar(12) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `pw` varchar(12) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(12) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `log`
+--
+
+CREATE TABLE `log` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `user_id` int(11) UNSIGNED NOT NULL,
+  `subiect_id` int(11) UNSIGNED NOT NULL,
+  `option_id` int(11) UNSIGNED NOT NULL,
+  `vote_name` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `optional`
+--
+
+CREATE TABLE `optional` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `option` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `subiect_id` int(11) NOT NULL,
+  `total` int(11) UNSIGNED NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `subjects`
+--
+
+CREATE TABLE `subjects` (
+  `id` int(11) UNSIGNED NOT NULL COMMENT '序號',
+  `subject` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '主題描述',
+  `type_id` int(11) NOT NULL,
+  `start` date NOT NULL,
+  `end` date NOT NULL,
+  `total` int(11) UNSIGNED NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `supervisor`
+--
+
+CREATE TABLE `supervisor` (
+  `id` int(11) UNSIGNED NOT NULL COMMENT '序號',
+  `acc` varchar(12) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '帳號account',
+  `pw` varchar(16) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(12) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `type`
+--
+
+CREATE TABLE `type` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `name` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `acc` varchar(12) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `pw` varchar(16) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(12) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `birthday` date NOT NULL,
+  `gender` tinyint(1) UNSIGNED NOT NULL,
+  `addr` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `eduation` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `reg_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `log`
+--
+ALTER TABLE `log`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `optional`
+--
+ALTER TABLE `optional`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `supervisor`
+--
+ALTER TABLE `supervisor`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `type`
+--
+ALTER TABLE `type`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
+--
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `log`
+--
+ALTER TABLE `log`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `optional`
+--
+ALTER TABLE `optional`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `supervisor`
+--
+ALTER TABLE `supervisor`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '序號';
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `type`
+--
+ALTER TABLE `type`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
